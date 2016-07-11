06f4504c0df994fcdb4024de53807cd715abee71
rocooshiang <rocoo.shaing@gmail.com>
2016-07-11 09:24:12 +0800

    Check two permissions.

--- Net_Tutorial/CameraApplication/app/src/main/java/rocoo/cameraapplication/MainActivity.java
+++ Net_Tutorial/CameraApplication/app/src/main/java/rocoo/cameraapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package rocoo.cameraapplication;
 
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -8,9 +12,10 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -20,8 +25,15 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    // For permissions request
+    private final int REQUEST_CAMERA_STORAGE = 1;
+
+    private final String[] CAMERA_STORAGE_PERMISSION = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
 
     private ImageView fullImageView, thumbnailImageView;
     private Button takePictureButton;
@@ -48,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
-                dispatchTakePictureIntent();
+                checkPermission();
             }
         });
 
@@ -69,6 +81,25 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case REQUEST_CAMERA_STORAGE:
+                // 判斷所有權限是否都被使用者所允許
+                if (PermissionUtil.verifyPermissions(grantResults)) {
+                    dispatchTakePictureIntent();
+                } else {
+                    Toast.makeText(this, "沒有權限，無法使用相機", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+    }
+
     private void dispatchTakePictureIntent(){
 
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -83,8 +114,8 @@ public class MainActivity extends AppCompatActivity {
                 photoFile = createImageFile();
             } catch (IOException ex) {
                 // Error occurred while creating the File
-                Toast toast = Toast.makeText(this, "在儲存照片時發生問題", Toast.LENGTH_SHORT);
-                toast.show();
+                Toast.makeText(this, "在儲存照片時發生問題", Toast.LENGTH_SHORT).show();
+
             }
 
             // Continue only if the File was successfully created
@@ -101,11 +132,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private File createImageFile() throws IOException {
+
+
         // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
+                Environment.DIRECTORY_DCIM);
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -149,6 +182,48 @@ public class MainActivity extends AppCompatActivity {
 
         Bitmap bitmap = BitmapFactory.decodeFile(imagePath, bmOptions);
         imageView.setImageBitmap(bitmap);
+    }
+
+    private void checkPermission() {
+
+        // 判斷所有權限是否都被允許
+        if (ActivityCompat.checkSelfPermission(this, CAMERA_STORAGE_PERMISSION[0]) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, CAMERA_STORAGE_PERMISSION[1]) != PackageManager.PERMISSION_GRANTED ) {
+
+            /** 沒有權限則顯示視窗告知使用者 **/
+
+            // 自訂視窗來解釋為何要有此權限，對使用者有什麼好處
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    CAMERA_STORAGE_PERMISSION[0]) || ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    CAMERA_STORAGE_PERMISSION[1])) {
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("權限通知")
+                        .setMessage("1. 掃描QRCode及拍照，需要取得您相機的權限 \n2. 照片儲存至相簿，需要取得您儲存的權限")
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                requestPermission(MainActivity.this, CAMERA_STORAGE_PERMISSION, REQUEST_CAMERA_STORAGE);
+                            }
+                        }).show();
+
+            } else {
+
+                // 顯示Android預設視窗 (安裝後第一次使用會跑這裡，使用者如果去關掉這個權限，那會跑上面)
+                requestPermission(MainActivity.this, CAMERA_STORAGE_PERMISSION, REQUEST_CAMERA_STORAGE);
+            }
+
+        } else {
+
+            /** The permissions are agree. **/
+
+            dispatchTakePictureIntent();
+        }
+    }
+
+    private void requestPermission(Activity activity, String[] permissions, int requestFrom) {
+
+        ActivityCompat.requestPermissions(activity, permissions, requestFrom);
     }
 }
 
